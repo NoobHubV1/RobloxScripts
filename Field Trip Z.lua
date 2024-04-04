f7b0b975fd054cf2089d404f3a2688489941783f
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-04-04 21:28:37 +0700

    Update Field Trip Z.lua

--- Field Trip Z.lua
+++ Field Trip Z.lua
@@ -1,7 +1,7 @@
 -- Place Check
 if game.PlaceId ~= 1701332290 then
 
-	local OrionLib = loadstring(Game:HttpGet('https://raw.githubusercontent.com/NoobHubV1/NoobHubV1/main/OrionLib.lua'))()
+	local OrionLib = loadstring(Game:HttpGet('https://raw.githubusercontent.com/NoobHubV1/RobloxScripts/main/OrionLib.lua'))()
 	local ReplicatedStorage = game:GetService("ReplicatedStorage")
 	local NetworkEvents = ReplicatedStorage:WaitForChild("NetworkEvents")
 	local Players = game:GetService("Players")
