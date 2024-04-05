c42d6e358143409318f35b7911fafa7f02f54dea
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-04-05 18:19:57 +0700

    Update Break In 2 Story Heal All Gui And More.lua

--- Break In 2 Story Heal All Gui And More.lua
+++ Break In 2 Story Heal All Gui And More.lua
@@ -51,9 +51,21 @@ end
 
 local function Heal(Heal)if Heal == "Heal All Players" then
 		                 HealAllPlayers()
-	                         elseif Heal == "Heal Yourself" then
+	                 elseif Heal == "Heal Yourself" then
 		                 HealYourself()
 	                         end
+end
+
+local function Train(Ability)Events:WaitForChild("RainbowWhatStat"):FireServer(Ability)
+end
+
+local function UnlockSecretEnding()SecretEnding = Events:WaitForChild("LarryEndingEvent")
+                                   SecretEnding:FireServer("HatCollected")
+	                           SecretEnding:FireServer("MaskCollected")
+	                           SecretEnding:FireServer("CrowbarCollected")
+end
+
+local function TeleportTo(CFrameArg)LocalPlayer.Character.HumanoidRootPart.CFrame = CFrameArg
 end
 
 local function Notify(Name, Content, Image, Time)game:GetService("StarterGui"):SetCore("SendNotification",{
@@ -80,6 +92,9 @@ local PhantomForcesWindow = Library:NewWindow("NoobHubV1 Hub")
 local BreakIn2 = PhantomForcesWindow:NewSection("Item Gui")
 
 BreakIn2:CreateDropdown("Item", {"Crowbar 1","Crowbar 2","Diamond Crowbar","Bat","Pitchfork","Hammer","Wrench","Broom","Armor","Med Kit","Key","Gold Key","Louise","Lollipop","Chips","Golden Apple","Pizza","Gold Pizza","Rainbow Pizza","Rainbow Pizza Box","Book","Phone","Cookie","Apple","Bloxy Cola","Expired Bloxy Cola","Bottle","Ladder","Battery"}, 2, function(Value)SelectedItem = Value
+end)
+
+BreakIn2:CreateTextbox("Item Textbox", function(Value)SelectedItem = Value
 end)
 
 BreakIn2:CreateButton("Get Item", function()GiveItem(SelectedItem)
@@ -116,6 +131,27 @@ end)
 local BreakIn2 = PhantomForcesWindow:NewSection("Remove Item")
 
 BreakIn2:CreateDropdown("Remove Item", {"Crowbar 1","Crowbar 2","Crowbar 3","Bat","Pitchfork","Hammer","Wrench","Broom","Med Kit","Key","Gold Key","Louise","Lollipop","Chips","Golden Apple","Pizza","Gold Pizza","Rainbow Pizza","Rainbow Pizza Box","Book","Phone","Cookie","Apple","Bloxy Cola","Expired Bloxy Cola","Bottle","Ladder","Battery"}, 2, function(Value)RemoveItem(Value)
+end)
+
+local BreakIn2 = PhantomForcesWindow:NewSection("Training")
+
+BreakIn2:CreateTextbox("Training Custom", function(Amount)CustomTraining = Amount
+end)
+
+BreakIn2:CreateDropdown("Train Ability", {"Strength","Speed"}, 2, function(Value)SelectedTrainAbility = Value
+end)
+
+BreakIn2:CreateButton("Train", function()Train(SelectedTrainAbility)
+end)
+
+BreakIn2:CreateButton("Unlock Secret Ending", function()UnlockSecretEnding()
+end)
+
+local PhantomForcesWindow = Library:NewWindow("Teleports")
+
+local BreakIn2 = PhantomForcesWindow:NewSection("Main")
+
+BreakIn2:CreateButton("Boss Fight", function()TeleportTo(CFrame.new(-1565.78772, -368.711945, -1040.66626, 0.0929690823, -1.97564436e-08, 0.995669007, -1.53269308e-08, 1, 2.1273511e-08, -0.995669007, -1.72383299e-08, 0.0929690823))
 end)
 
 Notify("Heal All Gui And More", "Loaded Script!", "rbxassetid://4483345998", 10)
