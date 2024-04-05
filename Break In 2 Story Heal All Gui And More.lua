4a943c10c41c846b90fc01ecb9e54909ef91e0ee
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-04-05 18:32:19 +0700

    Update Break In 2 Story Heal All Gui And More.lua

--- Break In 2 Story Heal All Gui And More.lua
+++ Break In 2 Story Heal All Gui And More.lua
@@ -13,13 +13,13 @@ local function UnequipAllTools()for i, v in pairs(LocalPlayer.Character:GetChild
 end
 
 local function GiveItem(Item)if Item == "Armor" then
-		             Events:WaitForChild("Vending"):FireServer(3, "Armor2", "Armor", tostring(LocalPlayer), 1)
+		                     Events:WaitForChild("Vending"):FireServer(3, "Armor2", "Armor", tostring(LocalPlayer), 1)
 	                     elseif Item == "Crowbar 1" or Item == "Crowbar 2" or Item == "Bat" or Item == "Pitchfork" or Item == "Hammer" or Item == "Wrench" or Item == "Broom" then
-		             Events:WaitForChild("Vending"):FireServer(3, tostring(Item:gsub(" ", "")), "Weapons", LocalPlayer.Name, 1)
+		                     Events:WaitForChild("Vending"):FireServer(3, tostring(Item:gsub(" ", "")), "Weapons", LocalPlayer.Name, 1)
 	                     elseif Item == "Diamond Crowbar" then
-		             Events:WaitForChild("Vending"):FireServer(3, "Crowbar3", "Weapons", LocalPlayer.Name, 1)
+		                     Events:WaitForChild("Vending"):FireServer(3, "Crowbar3", "Weapons", LocalPlayer.Name, 1)
 	                     else
-		             Events:WaitForChild("GiveTool"):FireServer(tostring(Item:gsub(" ", "")))
+		                     Events:WaitForChild("GiveTool"):FireServer(tostring(Item:gsub(" ", "")))
 	                     end
 end
 
@@ -53,19 +53,7 @@ local function Heal(Heal)if Heal == "Heal All Players" then
 		                 HealAllPlayers()
 	                 elseif Heal == "Heal Yourself" then
 		                 HealYourself()
-	                         end
-end
-
-local function Train(Ability)Events:WaitForChild("RainbowWhatStat"):FireServer(Ability)
-end
-
-local function UnlockSecretEnding()SecretEnding = Events:WaitForChild("LarryEndingEvent")
-                                   SecretEnding:FireServer("HatCollected")
-	                           SecretEnding:FireServer("MaskCollected")
-	                           SecretEnding:FireServer("CrowbarCollected")
-end
-
-local function TeleportTo(CFrameArg)LocalPlayer.Character.HumanoidRootPart.CFrame = CFrameArg
+	                 end
 end
 
 local function Notify(Name, Content, Image, Time)game:GetService("StarterGui"):SetCore("SendNotification",{
@@ -131,27 +119,6 @@ end)
 local BreakIn2 = PhantomForcesWindow:NewSection("Remove Item")
 
 BreakIn2:CreateDropdown("Remove Item", {"Crowbar 1","Crowbar 2","Crowbar 3","Bat","Pitchfork","Hammer","Wrench","Broom","Med Kit","Key","Gold Key","Louise","Lollipop","Chips","Golden Apple","Pizza","Gold Pizza","Rainbow Pizza","Rainbow Pizza Box","Book","Phone","Cookie","Apple","Bloxy Cola","Expired Bloxy Cola","Bottle","Ladder","Battery"}, 2, function(Value)RemoveItem(Value)
-end)
-
-local BreakIn2 = PhantomForcesWindow:NewSection("Training")
-
-BreakIn2:CreateTextbox("Training Custom", function(Amount)CustomTraining = Amount
-end)
-
-BreakIn2:CreateDropdown("Train Ability", {"Strength","Speed"}, 2, function(Value)SelectedTrainAbility = Value
-end)
-
-BreakIn2:CreateButton("Train", function()Train(SelectedTrainAbility)
-end)
-
-BreakIn2:CreateButton("Unlock Secret Ending", function()UnlockSecretEnding()
-end)
-
-local PhantomForcesWindow = Library:NewWindow("Teleports")
-
-local BreakIn2 = PhantomForcesWindow:NewSection("Main")
-
-BreakIn2:CreateButton("Boss Fight", function()TeleportTo(CFrame.new(-1565.78772, -368.711945, -1040.66626, 0.0929690823, -1.97564436e-08, 0.995669007, -1.53269308e-08, 1, 2.1273511e-08, -0.995669007, -1.72383299e-08, 0.0929690823))
 end)
 
 Notify("Heal All Gui And More", "Loaded Script!", "rbxassetid://4483345998", 10)
