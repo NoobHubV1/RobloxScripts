60c8d4cde95b8b5191f99e82ba7d24037ad58301
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-04-10 08:36:02 +0700

    Create Inf Jump.lua

---
+++ Inf Jump.lua
@@ -0,0 +1,44 @@
+local Library = loadstring(game:HttpGetAsync(("https://github.com/bloodball/-back-ups-for-libs/raw/main/wizard")))()
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Humanoid = LocalPlayer.Character.Humanoid
+local UserInputService = game:GetService("UserInputService")
+local JumpRequest = UserInputService.JumpRequest
+local ScriptLoaded = false
+
+local function InfJump(State)local InfJump = State
+JumpRequest:connect(function()
+if InfJump then
+Humanoid:ChangeState("Jumping")
+end
+end)
+end
+
+local function Notify(Name, Content, Time)game:GetService("StarterGui"):SetCore("SendNotification",{
+                Title = Name;
+                Text = Content;
+		Icon = "rbxassetid://4483345998";
+                Duration = Time;
+            })
+end
+Notify("Inf Jump", "Loading Script.", 3)
+
+                                                 wait(3)
+
+Notify("Inf Jump", "Loading Script..", 3)
+
+                                                 wait(3)
+
+Notify("Inf Jump", "Loading Script...", 3)
+
+                                                 wait(3)
+
+local PhantomForcesWindow = Library:NewWindow("NoobHubV1 Hub")
+
+local BreakIn2 = PhantomForcesWindow:NewSection("Inf Jump")
+
+BreakIn2:CreateToggle("Inf Jump", function(Value)InfJump(Value)
+end)
+
+Notify("Inf Jump", "Loaded Script!", 5)
+ScriptLoaded = true
