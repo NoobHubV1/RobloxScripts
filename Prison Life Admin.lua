81ed74c114264d9a2909f5c5aab17922ef9708b5
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-06-18 13:04:02 +0700

    Update Prison Life Admin.lua

--- Prison Life Admin.lua
+++ Prison Life Admin.lua
@@ -1,13 +1,13 @@
 Execution_Runtime = tick()
 PLadmin_Settings = {
   DefaultPrefix = ";"; --The default prefix for pladmin
-  JoinNotify = false; --Notify when a player joins
+  JoinNotify = true; --Notify when a player joins
   AutoRespawn = true; --Automatically loadcharacter when dying
-  AntiVoid = true; --Automatically teleport up when falling into void
+  AntiVoid = false; --Automatically teleport up when falling into void
   AntiTase = false; --Prevents you from being tased (100% no getconnections used because its absolute garbage)
   AntiArrest = false; --Prevents you from being arrested (100% no getconnections used because its absolute garbage)
   AntiShoot = false; --Kills player who tries to shoot you (Will be delayed if you have shitty ping, *COUGH* PLDT Users)
-  AntiPunch = false; --Instantly kill players who try to punch you
+  AntiPunch = true; --Instantly kill players who try to punch you
   AntiFling = false; --Prevent exploiters from flinging you
   AntiShield = false; --stop pay2win people and bypass their shields
   AntiBring = false; --Prevent other exploiter(s) from bringing you
@@ -24,4 +24,4 @@ PLadmin_Settings = {
   WhisperToRanked = true; --Use whisper for outputing commands for ranked players
   Force_isMobile = true; --Force mobile action gui (Punch and crawl buttons) if executor is too shitty and detected as "emulator"
 };wait() --Fixed on libhydrogen!!!
-loadstring(Game:HttpGet('https://gist.githubusercontent.com/NoobHubV1/62311c15965ed1c62a90c032a76ca08a/raw/e4732fe22d91ba9bff5fca883ae2dd0f9d02f42a/Prison%2520Life%2520Admin%2520Script%2520Fix.lua'))()
+loadstring(Game:HttpGet('https://gist.githubusercontent.com/NoobHubV1/b0256c15dfcd0860aa023dae5660984b/raw/a5edce2c66e75204bafdfbfdf895e1327598bbb4/Prison%2520Life%2520Script%2520Admin%2520Update.lua'))()
