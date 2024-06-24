061e7b1023b20792eaa624554c83bd496184a0e4
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-06-24 15:23:01 +0700

    Update SymphonyHub.lua

--- SymphonyHub.lua
+++ SymphonyHub.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet('https://github.com/NoobHubV1/ArrayField/blob/main/SymphonyHub.lua?raw=true'))()
+loadstring(game:HttpGet('https://github.com/NoobHubV1/ArrayField/raw/main/SymphonyHub.lua'))()
 -- Credits to Source Code Owner 
