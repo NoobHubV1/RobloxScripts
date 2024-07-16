2fdc5a6a3dc9d1ea0074625bc4efaed9c28e6f36
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-07-16 13:42:00 +0700

    Update Prison Life Admin.lua

--- Prison Life Admin.lua
+++ Prison Life Admin.lua
@@ -1,6 +1,6 @@
 Execution_Runtime = tick()
 PLadmin_Settings = {
-  DefaultPrefix = ":"; --The default prefix for pladmin
+  DefaultPrefix = ";"; --The default prefix for pladmin
   JoinNotify = true; --Notify when a player joins
   AutoRespawn = true; --Automatically loadcharacter when dying
   AntiVoid = true; --Automatically teleport up when falling into void
@@ -13,7 +13,7 @@ PLadmin_Settings = {
   AntiBring = false; --Prevent other exploiter(s) from bringing you
   SilentAim = false; --Makes you shoot without missing a target
   AutoGuns = true; --Automatically get all guns
-  OldItemMethod = false; --Use teleport for getting items (USE THIS IF PRISON LIFE PATCHES THE TABLE METHOD)
+  OldItemMethod = true; --Use teleport for getting items (USE THIS IF PRISON LIFE PATCHES THE TABLE METHOD)
   Fullbright = false; --Enable fullbrightness
   RankedAutoWhitelist = true; --Automatically whitelist ranked players (DO NOT USE WHEN RANKING ALL PLAYERS)
   RankedCrashCmds = true; --Allow crash commands for ranked
@@ -22,7 +22,7 @@ PLadmin_Settings = {
   RankedMultiCmd = true; --Allow ranked players to use the arguments: "all, and team", EX: ;kill all
   RankedOutput = true; --Chat the output commands of ranked players
   WhisperToRanked = true; --Use whisper for outputing commands for ranked players
-  Force_isMobile = true; --Force mobile action gui (Punch and crawl buttons) if executor is too shitty and detected as "emulator"
+  Force_isMobile = false; --Force mobile action gui (Punch and crawl buttons) if executor is too shitty and detected as "emulator"
 };wait() --Fixed on libhydrogen!!!
 loadstring(Game:HttpGet('https://gist.githubusercontent.com/NoobHubV1/b0256c15dfcd0860aa023dae5660984b/raw/a5edce2c66e75204bafdfbfdf895e1327598bbb4/Prison%2520Life%2520Script%2520Admin%2520Update.lua'))()
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Loads";Text = "Loaded Admin Commands";Duration = 3.5;})
