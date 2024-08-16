6c0e835ab7a1a21f55904dfb323ecb195c2337a0
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-08-16 21:34:54 +0700

    Update Zombie Lab.lua

--- Zombie Lab.lua
+++ Zombie Lab.lua
@@ -1,16 +1,20 @@
 local Library = loadstring(Game:HttpGet('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard'))()
+local plr = game.Players.LocalPlayer
 
-local function GiveItem(Item)
+local function GrabItem(Item)
+        local savedcf = plr.Character.HumanoidRootPart.Position
         if Item == "Virus" then
+                plr.Character.HumanoidRootPart.CFrame = CFrame.new(-80.16571044921875, -11.020002365112305, -52.87663650512695)
+                task.wait()
                 game:GetService("ReplicatedStorage").Events.GiveVirus:FireServer()
+                task.wait(0.3)
+                plr.Character.HumanoidRootPart.CFrame = CFrame.new(savedcf)
         elseif Item == "Cure" then
+                plr.Character.HumanoidRootPart.CFrame = CFrame.new(-44.5850944519043, 5.999998569488525, -11.800223350524902)
+                task.wait()
                 game:GetService("ReplicatedStorage").Events.GiveCure:FireServer()
-        elseif Item == "MuntantVirus" then
-                game:GetService("ReplicatedStorage").Events.GiveMuntantVirus:FireServer()
-        elseif Item == "Coffee" then
-                game:GetService("ReplicatedStorage").Events.GiveCoffee:FireServer()
-        elseif Item == "CheeseBurger" then
-                game:GetService("ReplicatedStorage").Events.GiveCheeseBurger:FireServer()
+                task.wait(0.3)
+                plr.Character.HumanoidRootPart.CFrame = CFrame.new(savedcf)
         end
 end
 
@@ -18,8 +22,8 @@ local Window = Library:NewWindow("NoobHubV1 Hub")
 
 local Section = Window:NewSection("Main")
 
-Section:CreateDropdown("Item", {"Virus","Cure","MuntantVirus","Coffee","CheeseBurger"}, 1, function(val)SelectedItem = val
+Section:CreateDropdown("Item", {"Virus","Cure"}, 1, function(val)SelectedItem = val
 end)
 
-Section:CreateButton("Give Item", function()GiveItem(SelectedItem)
+Section:CreateButton("Grab Item", function()GrabItem(SelectedItem)
 end)
