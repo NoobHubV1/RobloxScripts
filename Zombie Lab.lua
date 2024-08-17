2b8ad566872d7bc41e21a5c40f3ba9c7ee95b877
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-08-17 10:28:52 +0700

    Update Zombie Lab.lua

--- Zombie Lab.lua
+++ Zombie Lab.lua
@@ -54,6 +54,39 @@ local AutoGrabAll = function(Value)
         AutoGrabVirus(Value)
 end
 
+local function KillZombies()
+         --Equip Gun to Make Kill all work
+--- and Execute this script rapidly to Make all player in zombie died
+--- enjoy
+local player = game.Players.LocalPlayer
+for i,v in ipairs(game.Players:GetPlayers()) do
+  if v.Name ~= player.Name then
+    local BH = v.Character.Humanoid or v.Character:FindFirstChild("Humanoid")
+    local BK = v.Character.HumanoidRootPart or v.Character:FindFirsrChild("HumanoidRootPart")
+    local args = {
+    [1] = BH,
+    [2] = BK,
+    [3] = 10,
+    [4] = Vector3.new(0.9932631850242615, 0.018498189747333527, 0.11439403146505356),
+    [5] = 2,
+    [6] = 0,
+    [7] = false
+    }
+    game:GetService("Players").LocalPlayer.Character.Pistol.GunScript_Server.InflictTarget:FireServer(unpack(args))
+  end
+        end
+end
+
+local AutoKill = function(State)
+        plr.Character.Humanoid:EquipTool(plr.Backpack:FindFirstChild("Pistol"))
+        task.wait(0.3)
+        getgenv().Loop = State
+        while Loop do
+        KillZombies()
+        task.wait()
+        end
+end
+
 local Window = Library:NewWindow("NoobHubV1 Hub")
 
 local Section = Window:NewSection("Main")
@@ -73,5 +106,13 @@ end)
 Section:CreateButton("Grab All Items", function()GrabAll()
 end)
 
+Section:CreateButton("Virus All", function()VirusAll()
+end)
+
 Section:CreateToggle("Auto Grab All Items", function(val)AutoGrabAll(val)
+end)
+
+local Section = Window:NewSection("Zombie")
+
+Section:CreateToggle("Auto Kill Zombies", function(Value)AutoKill(Value)
 end)
