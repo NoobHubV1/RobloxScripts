fd21773f3b99c1f73fe1d17e84338311eef859f0
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-08-21 10:17:04 +0700

    Update Zombie Lab.lua

--- Zombie Lab.lua
+++ Zombie Lab.lua
@@ -87,7 +87,7 @@ local AutoKill = function(State)
         end
 end
 
-local function VirusPlayer()
+local function KillPlayer()
         -- Gui to Lua
 -- Version: 3.2
 
@@ -118,7 +118,7 @@ TextLabel.BackgroundTransparency = 1.000
 TextLabel.Position = UDim2.new(0.157534242, 0, 0, 0)
 TextLabel.Size = UDim2.new(0, 200, 0, 50)
 TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "Virus Player script"
+TextLabel.Text = "Kill Player script"
 TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
@@ -141,7 +141,7 @@ TextButton.BackgroundColor3 = Color3.fromRGB(170, 0, 255)
 TextButton.Position = UDim2.new(0.311643839, 0, 0.713450253, 0)
 TextButton.Size = UDim2.new(0, 109, 0, 39)
 TextButton.Font = Enum.Font.SourceSans
-TextButton.Text = "VIRUS"
+TextButton.Text = "KILL"
 TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextButton.TextScaled = true
 TextButton.TextSize = 14.000
@@ -149,7 +149,7 @@ TextButton.TextWrapped = true
 
 local plr = game.Players.LocalPlayer
 
-local function GetPlayer(String)
+function GetPlayer(String)
         if not String then return end
 	local Yes = {}
 	for _, Player in ipairs(game.Players:GetPlayers()) do
@@ -164,119 +164,20 @@ local function GetPlayer(String)
 	end
 end
 
-local function GiveVirus()
+local function GiveItem(Item)
+	if Item == "Virus" then
 	if game.Players.LocalPlayer.Character:FindFirstChild("Virus") or game.Players.LocalPlayer.Backpack:FindFirstChild("Virus") then
         -- nothing
 	else
         game.ReplicatedStorage.Events.GiveVirus:FireServer()
 	end
-end
-
-function ClickTool(Tool)
-	for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-if v.ToolTip == Tool then v.Parent = game.Players.LocalPlayer.Character end
-if game.Players.LocalPlayer.Character:FindFirstChildWhichIsA("Tool") then game.Players.LocalPlayer.Character:FindFirstChildWhichIsA("Tool"):Activate()
-end
-	end
-end
-
-TextButton.MouseButton1Click:Connect(function()
-GiveVirus()
-local Target = TextBox.Text
-for i = 1, 30 do
-plr.Character.Humanoid:EquipTool(plr.Backpack:FindFirstChild("Virus"))
-plr.Character.HumanoidRootPart.CFrame = GetPlayer(Target).Character.HumanoidRootPart.CFrame * CFrame.new(-2, 0, 2)
-ClickTool("Virus")
-task.wait()
-end
-task.wait(1)
-plr.Character.HumanoidRootPart.CFrame = CFrame.new(savedcf)
-end)
-end
-
-local function CurePlayer()
-        -- Gui to Lua
--- Version: 3.2
-
--- Instances:
-
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local TextLabel = Instance.new("TextLabel")
-local TextBox = Instance.new("TextBox")
-local TextButton = Instance.new("TextButton")
-
---Properties:
-
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(0, 85, 255)
-Frame.Position = UDim2.new(0.0799492374, 0, 0.483815432, 0)
-Frame.Size = UDim2.new(0, 292, 0, 171)
-Frame.Active = true
-Frame.Archivable = true
-Frame.Draggable = true
-
-TextLabel.Parent = Frame
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.Position = UDim2.new(0.157534242, 0, 0, 0)
-TextLabel.Size = UDim2.new(0, 200, 0, 50)
-TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "Cure Player script"
-TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.TextScaled = true
-TextLabel.TextSize = 14.000
-TextLabel.TextStrokeTransparency = 0.000
-TextLabel.TextWrapped = true
-
-TextBox.Parent = Frame
-TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextBox.Position = UDim2.new(0.157534242, 0, 0.350877196, 0)
-TextBox.Size = UDim2.new(0, 200, 0, 50)
-TextBox.Font = Enum.Font.SourceSans
-TextBox.Text = ""
-TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextBox.TextScaled = true
-TextBox.TextSize = 14.000
-TextBox.TextWrapped = true
-
-TextButton.Parent = Frame
-TextButton.BackgroundColor3 = Color3.fromRGB(170, 0, 255)
-TextButton.Position = UDim2.new(0.311643839, 0, 0.713450253, 0)
-TextButton.Size = UDim2.new(0, 109, 0, 39)
-TextButton.Font = Enum.Font.SourceSans
-TextButton.Text = "CURE"
-TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton.TextScaled = true
-TextButton.TextSize = 14.000
-TextButton.TextWrapped = true
-
-local plr = game.Players.LocalPlayer
-
-local function GetPlayer(String)
-        if not String then return end
-	local Yes = {}
-	for _, Player in ipairs(game.Players:GetPlayers()) do
-		if string.lower(Player.Name):match(string.lower(String)) or string.lower(Player.DisplayName):match(string.lower(String)) then
-			table.insert(Yes, Player)
-		end
-	end
-	if #Yes > 0 then
-		return Yes[1]
-	elseif #Yes < 1 then
-		return nil
-	end
-end
-
-local function GiveCure()
+	elseif Item == "Cure" then
 	if game.Players.LocalPlayer.Character:FindFirstChild("Cure") or game.Players.LocalPlayer.Backpack:FindFirstChild("Cure") then
         -- nothing
 	else
         game.ReplicatedStorage.Events.GiveCure:FireServer()
 	end
+	end
 end
 
 function ClickTool(Tool)
@@ -288,17 +189,24 @@ end
 end
 
 TextButton.MouseButton1Click:Connect(function()
-GiveCure()
-local savedcf = plr.Character.HumanoidRootPart.Position
 local Target = TextBox.Text
-for i = 1, 30 do
+if GetPlayer(Target).Team == game.Teams.Human then
+GiveItem("Virus")
+for i = 1, 25 do
+plr.Character.Humanoid:EquipTool(plr.Backpack:FindFirstChild("Virus"))
+plr.Character.HumanoidRootPart.CFrame = GetPlayer(Target).Character.HumanoidRootPart.CFrame * CFrame.new(-2, 0, 2)
+ClickTool("Virus")
+task.wait()
+end
+elseif GetPlayer(Target).Team == game.Teams.Zombie then
+GiveItem("Cure")
+for i = 1, 25 do
 plr.Character.Humanoid:EquipTool(plr.Backpack:FindFirstChild("Cure"))
 plr.Character.HumanoidRootPart.CFrame = GetPlayer(Target).Character.HumanoidRootPart.CFrame * CFrame.new(-2, 0, 2)
 ClickTool("Cure")
 task.wait()
 end
-task.wait(1)
-plr.Character.HumanoidRootPart.CFrame = CFrame.new(savedcf)
+end
 end)
 end
 
@@ -331,8 +239,5 @@ end)
 
 local Section = Window:NewSection("Others")
 
-Section:CreateButton("Virus Player", function()VirusPlayer()
-end)
-
-Section:CreateButton("Cure Player", function()CurePlayer()
+Section:CreateButton("Kill Player", function()KillPlayer()
 end)
