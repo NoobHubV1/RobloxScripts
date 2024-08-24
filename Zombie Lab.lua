768cdaf29b9348218945035e50ba8368a751da83
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-08-24 11:57:25 +0700

    Update Zombie Lab.lua

--- Zombie Lab.lua
+++ Zombie Lab.lua
@@ -1,5 +1,63 @@
+--MADE BY NOOBHUBV1
+
 local Library = loadstring(Game:HttpGet('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard'))()
 local plr = game.Players.LocalPlayer
+
+local function Tween(Obj, Prop, New, Time)
+	if not Time then
+		Time = .5
+	end
+	local TweenService = game:GetService("TweenService")
+	local info = TweenInfo.new(
+		Time, 
+		Enum.EasingStyle.Quart, 
+		Enum.EasingDirection.Out, 
+		0, 
+		false,
+		0
+	)
+	local propertyTable = {
+		[Prop] = New,
+	}
+
+	TweenService:Create(Obj, info, propertyTable):Play()
+end
+
+local function Notify(Text,Dur)
+	task.spawn(function()
+		if not Dur then
+			Dur = 1.5
+		end
+		local Notif = Instance.new("ScreenGui")
+		local Frame_1 = Instance.new("Frame")
+		local TextLabel = Instance.new("TextLabel")
+		Notif.Parent = (game:GetService("CoreGui") or gethui())
+		Notif.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+		Frame_1.Parent = Notif
+		Frame_1.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+		Frame_1.BackgroundTransparency=1
+		Frame_1.BorderSizePixel = 0
+		Frame_1.Position = UDim2.new(0, 0, 0.0500000007, 0)
+		Frame_1.Size = UDim2.new(1, 0, 0.100000001, 0)
+		TextLabel.Parent = Frame_1
+		TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		TextLabel.BackgroundTransparency = 1.000
+		TextLabel.TextTransparency =1
+		TextLabel.Size = UDim2.new(1, 0, 1, 0)
+		TextLabel.Font = Enum.Font.Highway
+		TextLabel.Text = Text or "Text not found"
+		TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+		TextLabel.TextSize = 21.000
+		Tween(Frame_1,"BackgroundTransparency",0.350,.5)
+		Tween(TextLabel,"TextTransparency",0,.5)
+		wait(Dur+.7)
+		Tween(Frame_1,"BackgroundTransparency",1,.5)
+		Tween(TextLabel,"TextTransparency",1,.5)
+		wait(.7)
+		Notif:Destroy()
+	end)
+	return
+end
 
 local function GrabItem(Item)
         local savedcf = plr.Character.HumanoidRootPart.Position
@@ -72,7 +130,7 @@ for i,v in ipairs(game.Players:GetPlayers()) do
     [6] = 0,
     [7] = false
     }
-    game:GetService("Players").LocalPlayer.Character.Pistol.GunScript_Server.InflictTarget:FireServer(unpack(args))
+    game:GetService("Players").LocalPlayer.Character:FindFirstChildWhichIsA("Tool").GunScript_Server.InflictTarget:FireServer(unpack(args))
   end
         end
 end
@@ -126,3 +184,5 @@ local Section = Window:NewSection("Others")
 
 Section:CreateButton("Cure/Virus Player", function()ChangeTeamPlayer()
 end)
+
+Notif("(Zombie Lab) Script Loaded!",5)
