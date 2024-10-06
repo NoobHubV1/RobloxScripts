66b4b5dd36a50da0127b2ded6c147ec41d5dc8f5
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-10-06 15:58:57 +0700

    Update Sonic Ultimate RPG.lua

--- Sonic Ultimate RPG.lua
+++ Sonic Ultimate RPG.lua
@@ -1,32 +1,26 @@
 local Library = loadstring(Game:HttpGet('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard'))()
 
 local GetEmerald = function(Color)
-        local LastP = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.ChaosEmeralds:FindFirstChild(Color, true).CFrame
-        task.wait()
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = LastP
+        firetouchinterest(game.Players.LocalPlayer.Character.Head, workspace.ChaosEmeralds:FindFirstChild(Color, true), 0)
 end
 
 local GetAllEmeralds = function()
         GetEmerald("Blue")
-        wait(.5)
         GetEmerald("Yellow")
-        wait(.5)
-        GetEmerald("Cyan")
-        wait(.5)
-        GetEmerald("Purple")
-        wait(.5)
-        GetEmerald("Green")
-        wait(.5)
-        GetEmerald("White")
-        wait(.5)
         GetEmerald("Red")
+        GetEmerald("Cyan")
+        GetEmerald("Purple")
+        GetEmerald("White")
+        GetEmerald("Green")
 end
 
 local function GetAllRings()
         for i,v in pairs(game:GetService("Workspace").Rings:GetDescendants()) do
 if v:IsA('Part') then
-        firetouchinterest(game.Players.LocalPlayer.Character:FindFirstChildWhichIsA("Part"), v, 0)
+        local saved = v.CFrame
+        v.CFrame = game.Players.LocalPlayer.Character.Head.CFrame
+        task.wait(0.03)
+        v.CFrame = saved
 end
 end
 end
@@ -39,9 +33,6 @@ Section:CreateDropdown("Emerald Color", {"Blue","Cyan","Green","Purple","Red","W
 end)
 
 Section:CreateButton("Get Emerald", function()GetEmerald(SelectedEmeraldsColor)
-end)
-
-Section:CreateButton("Get Rings", function()firetouchinterest(game.Players.LocalPlayer.Character:FindFirstChildWhichIsA("Part"), game.Workspace.Rings, 0)
 end)
 
 local Section = Window:NewSection("All")
