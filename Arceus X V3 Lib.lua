c81d5fef18da66b875ceca8ac170a8b5a2926346
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-10-14 10:23:15 +0700

    Update Arceus X V3 Lib.lua

--- Arceus X V3 Lib.lua
+++ Arceus X V3 Lib.lua
@@ -1,6 +1,4 @@
 local lib = {}
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/FilteringEnabled/FE/main/notificationtest"))();
-local Notify = Notification.Notify;
 
 local Script_Title = "Arceus X <font color=\"rgb(255, 0, 0)\">|</font> Ui Lib"
 
@@ -533,12 +531,56 @@ function lib:Destroy()
     Arceus:Destroy()
 end
 
-function lib:Notify(Title, Text, Time)
-	Notify({
-		 Description = Text;
-		 Title = Title;
-		 Duration = Time;
-	});
+function Tween(Obj, Prop, New, Time)
+	if not Time then
+		Time = .5
+	end
+	local TweenService = game:GetService("TweenService")
+	local info = TweenInfo.new(
+		Time, 
+		Enum.EasingStyle.Quart, 
+		Enum.EasingDirection.Out, 
+		0, 
+		false,
+		0
+	)
+	local propertyTable = {
+		[Prop] = New,
+	}
+
+	TweenService:Create(Obj, info, propertyTable):Play()
+end
+function lib:SetNotification(Text, Color, Title, Time)
+	task.spawn(function()
+		if not Time then
+			Time = 1.5
+		end
+		local Frame_1 = Instance.new("Frame")
+		local TextLabel = Instance.new("TextLabel")
+		Frame_1.Parent = Arceus
+		Frame_1.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+		Frame_1.BackgroundTransparency=1
+		Frame_1.BorderSizePixel = 0
+		Frame_1.Position = UDim2.new(0, 0, 0.0500000007, 0)
+		Frame_1.Size = UDim2.new(1, 0, 0.100000001, 0)
+		TextLabel.Parent = Frame_1
+		TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		TextLabel.BackgroundTransparency = 1.000
+		TextLabel.TextTransparency =1
+		TextLabel.Size = UDim2.new(1, 0, 1, 0)
+		TextLabel.Font = Enum.Font.Highway
+		TextLabel.Text = "["..Title.."] "..Text
+		TextLabel.TextColor3 = Color3.fromRGB(Color)
+		TextLabel.TextSize = 21.000
+		Tween(Frame_1,"BackgroundTransparency",0.350,.5)
+		Tween(TextLabel,"TextTransparency",0,.5)
+		wait(Time+.7)
+		Tween(Frame_1,"BackgroundTransparency",1,.5)
+		Tween(TextLabel,"TextTransparency",1,.5)
+		wait(.7)
+		Notif:Destroy()
+	end)
+	return
 end
 
 function lib:SetBackgroundColor(r, g ,b)
