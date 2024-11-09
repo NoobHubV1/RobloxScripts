943a88f6c9d88011ccdc491a549bf07d65705544
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-11-09 07:54:48 +0700

    Update Arceus X V3 Lib.lua

--- Arceus X V3 Lib.lua
+++ Arceus X V3 Lib.lua
@@ -53,7 +53,6 @@ Arceus.DisplayOrder = 999999999
 Main.Name = "Main"
 Main.Parent = Arceus
 Main.Active = true
-Main.Draggable = true
 Main.AnchorPoint = Vector2.new(0.5, 0.5)
 Main.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
 Main.BorderSizePixel = 0
@@ -320,6 +319,8 @@ UIAspectRatioConstraint_5.Parent = Img_2
 -- SCRIPT
 
 local TweenService = game:GetService("TweenService")
+local Notification = loadstring(Game:HttpGet("https://raw.githubusercontent.com/NoobHubV1/NoobHubV1/main/Notification%20Lib.lua"))()
+local DraggableGuis = true
 Close.MouseButton1Click:Connect(function()
 	Logo.Active = true
 	TweenService:Create(Intro, TweenInfo.new(0.25, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {BackgroundTransparency = 0}):Play()
@@ -519,26 +520,6 @@ function lib:AddDropdown(text, options, funct, ...) -- ADD CUSTOM ELEMENT INSTEA
 	return newCombo
 end
 
-function Tween(Obj, Prop, New, Time)
-	if not Time then
-		Time = .5
-	end
-	local TweenService = game:GetService("TweenService")
-	local info = TweenInfo.new(
-		Time, 
-		Enum.EasingStyle.Quart, 
-		Enum.EasingDirection.Out, 
-		0, 
-		false,
-		0
-	)
-	local propertyTable = {
-		[Prop] = New,
-	}
-
-	TweenService:Create(Obj, info, propertyTable):Play()
-end
-
 function lib:MakeNotification(NotifConfig)
 	task.spawn(function()
 		NotifConfig.Name = NotifConfig.Name or "Title not found!"
@@ -546,36 +527,13 @@ function lib:MakeNotification(NotifConfig)
 		NotifConfig.Color = NotifConfig.Color or Color3.fromRGB(255, 255, 255)
 		NotifConfig.Time = NotifConfig.Time or 5
 
-		local Notif = Instance.new("ScreenGui")
-		local Frame_1 = Instance.new("Frame")
-		local TextLabel = Instance.new("TextLabel")
-		Notif.Name = math.random()
-		Notif.Parent = (game:GetService("CoreGui") or gethui())
-		Notif.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-		Frame_1.Parent = Notif
-		Frame_1.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-		Frame_1.BackgroundTransparency=1
-		Frame_1.BorderSizePixel = 0
-		Frame_1.Position = UDim2.new(0, 0, 0.0500000007, 0)
-		Frame_1.Size = UDim2.new(1, 0, 0.100000001, 0)
-		TextLabel.Parent = Frame_1
-		TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		TextLabel.BackgroundTransparency = 1.000
-		TextLabel.TextTransparency =1
-		TextLabel.Size = UDim2.new(1, 0, 1, 0)
-		TextLabel.Font = Enum.Font.Highway
-		TextLabel.Text = "["..NotifConfig.Name.."] "..NotifConfig.Content
-		TextLabel.TextColor3 = NotifConfig.Color
-		TextLabel.TextSize = 21.000
-		Tween(Frame_1,"BackgroundTransparency",0.350,.5)
-		Tween(TextLabel,"TextTransparency",0,.5)
-		wait(NotifConfig.Time+.7)
-		Tween(Frame_1,"BackgroundTransparency",1,.5)
-		Tween(TextLabel,"TextTransparency",1,.5)
-		wait(.7)
-		Notif:Destroy()
+		Notification:MakeNotification({
+			Name = NotifConfig.Name,
+			Content = NotifConfig.Content,
+			Color = NotifConfig.Color,
+			Time = NotifConfig.Time
+		})
 	end)
-	return
 end
 
 function lib:SetTitle(txt)
@@ -635,6 +593,58 @@ function lib:SetTheme(theme)
 		error("Theme not found.")
 	end
 end
+
+function DragifyGui(Frame,Is)
+	coroutine.wrap(function()
+		local dragToggle = nil
+		local dragSpeed = 5
+		local dragInput = nil
+		local dragStart = nil
+		local dragPos = nil
+		local startPos = nil
+		local function updateInput(input)
+			if not Is then
+				if not DraggableGuis then
+					return
+				end
+			end
+			local Delta = input.Position - dragStart
+			local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
+			game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.30), {Position = Position}):Play()
+		end
+		Frame.InputBegan:Connect(function(input)
+			if not Is then
+				if DraggableGuis then
+					if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and game:GetService("UserInputService"):GetFocusedTextBox() == nil then
+						dragToggle = true
+						dragStart = input.Position
+						startPos = Frame.Position
+						input.Changed:Connect(function()
+							if input.UserInputState == Enum.UserInputState.End then
+								dragToggle = false
+							end
+						end)
+					end
+				end
+			end
+		end)
+		Frame.InputChanged:Connect(function(input)
+			if DraggableGuis then
+				if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+					dragInput = input
+				end
+			end
+		end)
+		game:GetService("UserInputService").InputChanged:Connect(function(input)
+			if DraggableGuis then
+				if input == dragInput and dragToggle then
+					updateInput(input)
+				end
+			end
+		end)
+	end)()
+end
+DragifyGui(Main)
 -- INIT
 
 Main:TweenPosition(
